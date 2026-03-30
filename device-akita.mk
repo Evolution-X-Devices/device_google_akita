7dad8496c96ecf21501e201802f31d4280d75856
TechPanelGM <giomook2003@gmail.com>
2026-03-30 13:16:23 +0000

    akita: Welcome to Evolution X

--- device-akita.mk
+++ device-akita.mk
@@ -26,6 +26,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 # Overlays
+DEVICE_PACKAGE_OVERLAYS += device/google/akita/overlay-evolution
+
 PRODUCT_PACKAGES += \
     FrameworkResOverlayProductAkita \
     FrameworkResOverlayVendorAkita \
